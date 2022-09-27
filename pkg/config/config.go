a39335bbef552e0ad6921f7a64249db2d6b9379d
Nick Mancari <nickjmancari@gmail.com>
2022-09-27 15:54:32 -0500

    config pkg creation with setters and getters

---
+++ pkg/config/config.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+)
+
+/* config.json file will be created at installation with bash script at /etc/doofer/config.json
+ */
+
+const config_file = "/etc/doofer/config.json"
+
+type Setting struct {
+	Downloads string
+	Media     MediaFolders
+	Vpn       bool
+}
+
+type MediaFolders struct {
+	TV     string
+	Movies string
+}
+
+// Get the Download location from the config file
+func (s Setting) DownloadSetting() string {
+
+	return s.Downloads
+
+}
+
+/*
+func (s Setting) MediaSettings() string {
+
+
+
+}
+*/
+func (s Setting) VpnSettings() bool {
+
+	return s.Vpn
+
+}
+
+// Read the config file from JSON format to use with the getters
+func Read() *Setting {
+
+	jsonFile, err := ioutil.ReadFile(config_file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var setting Setting
+
+	err = json.Unmarshal(jsonFile, &setting)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return &setting
+
+}
