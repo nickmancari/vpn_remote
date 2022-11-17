256e672d0b7aba47e783f97a30a6481c0c0053a7
Nick Mancari <nickjmancari@gamil.com>
2022-11-17 15:23:38 -0600

    starter unit tests for system unit

--- internal/sys/system_test.go
+++ internal/sys/system_test.go
@@ -9,6 +9,24 @@ func TestVpnSettingStatus(t *testing.T) {
 	want := false
 
 	if want != got {
-		t.Errorf("expected %v, but got %v", want, got)
+		t.Errorf("eExpected '%v', but got '%v'", want, got)
+	}
+}
+
+func TestInternetProtocolAddress(t *testing.T) {
+	got := InternetProtocolAddress()
+	want := "IP Address Unaviable"
+
+	if got != want {
+		t.Errorf("Expected '%v', but got '%v'", want, got)
+	}
+}
+
+func TestVpnDaemonStatus(t *testing.T) {
+	got := VpnDaemonStatus()
+	want := "VPN Status Unknown"
+
+	if got != want {
+		t.Errorf("Expected %v, but got %v", want, got)
 	}
 }
