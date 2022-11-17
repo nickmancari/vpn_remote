636f35bcad8250d070aa85e2a7be92486ee5bba0
Nick Mancari <nickjmancari@gamil.com>
2022-11-17 11:18:34 -0600

    VpnSettingStatus() test

--- internal/sys/system_test.go
+++ internal/sys/system_test.go
@@ -1,1 +1,14 @@
 package sys
+
+import (
+	"testing"
+)
+
+func TestVpnSettingStatus(t *testing.T) {	
+	got := VpnSettingStatus()
+	want := false
+
+	if want != got {
+		t.Errorf("expected %v, but got %v", want, got)
+	}
+}
